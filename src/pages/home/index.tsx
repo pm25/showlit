9c44d2bd194dc04e2ba0267cc1ba88ac394bd194
py <pyhuang97@gmail.com>
2025-11-25 12:55:47 -0700

    [update] adjust gap space on homepage.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
   usePageTitle("About Me");
 
   return (
-    <div className="flex flex-1 flex-col items-center gap-16 my-4">
+    <div className="flex flex-1 flex-col items-center gap-24 my-4">
       {homepage.sections.map((sectionName) => (
         <div key={sectionName} className="w-full max-w-5xl px-2 md:px-8">
           {sectionComponents[sectionName]}
