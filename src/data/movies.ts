6d8ba8cbf0dcb1fbc7e8db8a19781226c8f257ad
py <pyhuang97@gmail.com>
2025-11-24 23:05:03 -0700

    [update] manage movie list with YAML.

---
+++ src/data/movies.ts
@@ -0,0 +1,22 @@
+export const movies = [
+  {
+    title: `The Shawshank Redemption`,
+    posterImage: `https://media.themoviedb.org/t/p/w440_and_h660_face/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg`,
+    note: `A timeless story of hope and perseverance that reminds me to stay resilient, no matter the odds.`
+  },
+  {
+    title: `Schindler's List`,
+    posterImage: `https://image.tmdb.org/t/p/original/doGEE2DgjET0XK0k9BozsMBES5H.jpg`,
+    note: `A powerful portrayal of courage and humanity in the face of unimaginable adversity.`
+  },
+  {
+    title: `Interstellar`,
+    posterImage: `https://image.tmdb.org/t/p/original/mS4EvhsrT0SQZOlWrQEzWI5KiUa.jpg`,
+    note: `Blends science, love, and human ambition in a way that sparks my curiosity about the universe and our place in it.`
+  },
+  {
+    title: `La La Land`,
+    posterImage: `https://image.tmdb.org/t/p/original/pfr9dwWqANfF6XohNZ1ANtcXSq2.jpg`,
+    note: `A beautiful reminder of dreams, sacrifice, and the bittersweet balance between passion and reality.`
+  }
+];
