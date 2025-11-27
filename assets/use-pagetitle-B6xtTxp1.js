93e3f4bc8a6930d36ca482e71a26d22459811bcd
pm25 <pm25@users.noreply.github.com>
2025-11-27 08:14:41 +0000

    [ci] deploy site c53b618bc88817cdc815f9cca8760b3c03084ae2

---
+++ assets/use-pagetitle-B6xtTxp1.js
@@ -0,0 +1,1 @@
+import{r as t,J as s}from"./index-mSYnv4Fs.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-BBPTYCvi.js
+++
@@ -1,1 +0,0 @@
-import{r as t,J as s}from"./index-RPcA6b7z.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
