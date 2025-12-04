ca388a17c9912623075f555cd7757aea15af4ded
pm25 <pm25@users.noreply.github.com>
2025-12-04 20:47:49 +0000

    [ci] deploy site 419b6d0d6d6df87a6356a0557f8bd73a66f14e89

--- assets/use-pagetitle-DcirtpBN.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-Pmft3z0z.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
---
+++ assets/use-pagetitle-DkSSlD4T.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-ChaLGSX1.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
